aab23fc3fa080a3e77991bc18cb6a5adca48ddbf
Johan Kielbaey <johan@kielbaey-oliveros.eu>
2017-04-15 18:12:58 +0200

    Update bootstrap file.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,90 +1,87 @@
 #!/bin/bash
 
+do_upgrade=0
+while [ $# -ge 1 ]; do
+    key="$1"
+    case $key in
+        -u|--upgrade)
+            do_upgrade=1
+            echo "-- Doing upgrades.\n"
+            ;;
+    esac
+    shift
+done
+
 OS=`uname`
 if [ "$OS" == "Darwin" ]; then
-    echo "Bootstrapping on MAC OS X..."
+    echo "Bootstrapping on MAC OS X...\n"
+
+    BASEDIR=`pwd`/`dirname $0`
+    echo "-- Git repo cloned into $BASEDIR.\n"
+
     if [ ! -f /usr/local/bin/brew ]; then
         echo "Brew is not installed. Let's install that first."
         /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    echo ""
     fi
 
     echo "# Updating brew and pouring some packages..."
     brew update
-    brew upgrade
-    brew install python --upgrade
-    brew install python3 --upgrade
-    brew install ruby --upgrade
-    brew install perl --upgrade
-    brew install git --upgrade
-    brew install fish --upgrade
-    brew install httpie --upgrade
-    brew install vim --with-python --with-ruby --with-perl --upgrade
-    brew install macvim --env-std --with-override-system-vim --upgrade
-    brew link --overwrite macvim
+    brew install cmake python python3 ruby perl git fish httpie
+    brew install the_silver_searcher wget htop nmap ipcalc pidof
+    brew install tree
+    brew tap neovim/neovim
+    brew install neovim
+    brew tap caskroom/versions
+    brew cask install java iterm2
+    [ $do_upgrade -eq 1 ] && brew upgrade
     brew cleanup
+    echo ""
 
-    echo "# Allow use of fish as shell"
     if ! grep -q '/usr/local/bin/fish' /etc/shells; then
-        echo '/usr/local/bin/fish' >> /etc/shells
+        echo "# Allow use of fish as shell"
+        echo '/usr/local/bin/fish' | sudo tee -a /etc/shells >/dev/null 2>&1
+        echo ""
     fi
 
     echo "# Installing/upgrading python packages..."
-    pip install pip --upgrade
-    pip install powerline-status --upgrade
+    pip install --upgrade pip setuptools powerline-status awscli ansible
+    pip install --upgrade neovim pip-tools psutil bottle glances
+    echo ""
 
     echo "# Install powerline fonts..."
-    sh ~/mydots/powerline_fonts/install.sh
+    if [ -d $BASEDIR/powerline ]; then
+        cd $BASEDIR/powerline
+        git pull
+        cd $OLDPWD
+    else
+        git clone https://github.com/powerline/fonts.git $BASEDIR/powerline
+    fi
+    sh $BASEDIR/powerline/install.sh
+    echo ""
 
-    echo "# Create symlinks for .vim, .vimrc and .config"
-    for i in .vim .vimrc .config; do
+    echo "# Install vim-plug."
+    if [ ! -e $BASEDIR/.config/nvim/autoload/plug.vim ]; then
+        curl -fLo $BASEDIR/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    fi
+
+    echo "# Create .config symlink."
+    for i in .ansible.cfg .gitignore .config; do
         if [ -e ~/${i} ]; then
+            echo "-- $i"
             if [ -e ~/${i}_before_bootstrap ]; then
                 rm -r ~/${i}
             else
                 mv ~/${i} ~/${i}_before_bootstrap
             fi
         fi
-        ln -s ~/mydots/${i} .
+        ln -s $BASEDIR/${i} ~/${i}
     done
 
-    echo "# Install VIM plugins..."
-    vim +PluginInstall +qall
+    echo "# Install vim plugins."
+    nvim +PlugInstall +qall
 
 elif [ "$OS" == "Linux" ]; then
-    echo "Bootstrapping on Linux..."
-
-    echo "# Installing packages..."
-    PACKAGES="python python3 vim-enhanced perl git19 curl python-pip python-setuptools"
-    sudo yum -y install $PACKAGES >/dev/null 2>&1
-    for pkg in $PACKAGES; do
-        rpm -q $pkg >/dev/null 2>&1 || echo "Error! Failed to install package $pkg."
-    done
-
-    [ -f /etc/yum.repos.d/fish.repo ] && sudo rm /etc/yum.repos.d/fish.repo
-    curl http://fishshell.com/files/linux/RedHat_RHEL-6/fish.release:2.repo 2>/dev/null | sudo tee /etc/yum.repos.d/fish.repo >/dev/null
-    sudo yum -y install fish >/dev/null 2>&1
-    rpm -q fish >/dev/null 2>&1 || echo "Error! Failed to install package fish."
-
-    echo "# Installing/upgrading python packages..."
-    pip install pip --upgrade
-    pip install powerline-status --upgrade
-
-    echo "# Install powerline fonts..."
-    sh ~/mydots/powerline_fonts/install.sh
-
-    echo "# Create symlinks for .vim, .vimrc and .config"
-    for i in .vim .vimrc .config; do
-        if [ -e ~/${i} ]; then
-            if [ -e ~/${i}_before_bootstrap ]; then
-                rm -r ~/${i}
-            else
-                mv ~/${i} ~/${i}_before_bootstrap
-            fi
-        fi
-        ln -s ~/mydots/${i} .
-    done
-
-    echo "# Install VIM plugins..."
-    vim +PluginInstall +qall
-
+    echo "Bootstrapping on Linux... no longer supported."
+    exit 1
 fi
